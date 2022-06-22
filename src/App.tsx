4d707ea7ee2fb693d94423461c112406d45c6fff
Jonathan Thomsen <jthomsen.itj@gmail.com>
2022-06-21 23:35:41 -0300

    feat: adicionado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Cadastros</h1>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
